584111bf5f2fa7baded2614503458d850cc534e1
Victor <samuelvictoralexander@gmail.com>
2025-07-16 14:46:19 +0100

    implemented light mode hover and darkmode

--- src/app/component/CoordinatorCard.tsx
+++ src/app/component/CoordinatorCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function CoordinatorCard({ coordinator }: { coordinator: any }) {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-4">
+    <div className="bg-white dark:bg-gray-800  rounded-lg shadow p-4 ">
       <Image
         src={coordinator.photo}
         width={400}
@@ -17,7 +17,7 @@ export default function CoordinatorCard({ coordinator }: { coordinator: any }) {
       <p className="text-gray-600 dark:text-gray-300">{coordinator.location}</p>
       <p className="text-green-600 font-bold">{coordinator.price}</p>
       <Link href={`/coordinator/${coordinator.id}`}>
-        <button className="mt-3 bg-blue-600 text-white px-4 py-2 rounded text-base font-normal cursor-pointer hover:bg-transparent border-neutral border transition-all duration-300">
+        <button className="mt-3 bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded text-base font-normal cursor-pointer dark:hover:bg-transparent dark:border-neutral border transition-all duration-300">
           View Profile
         </button>
       </Link>
