2c154b70ca8bb24d26e806ca762a027c37b2117c
Victor <samuelvictoralexander@gmail.com>
2025-07-31 14:36:36 +0100

    added 404 and loading page

---
+++ src/app/component/Preloader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HashLoader from "react-spinners/HashLoader";
+
+export default function Preloader() {
+  return (
+    <div className="z-[13000] fixed bg-black inset-0 w-full h-full grid place-items-center">
+      <HashLoader
+        color="#2b374d"
+        size={80}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import Preloader from "@/app/component/Preloader";
+
+export default function Loading() {
+  return <Preloader />;
+}
